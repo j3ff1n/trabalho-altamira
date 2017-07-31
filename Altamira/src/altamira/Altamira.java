9929b25544b2ce5f01f88d84e37d7aa7598fccd7
Maxwel e Joice <Maxwel e Joice@MaxwelJoice>
2017-07-31 12:04:30 -0300

    Merge origin/master
    Conflicts:
    	testeClone/src/testeclone/TesteClone.java

---
+++ Altamira/src/altamira/Altamira.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package altamira;
+
+/**
+ *
+ * @author Maxwel e Joice
+ */
+public class Altamira {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+System.out.println("trabalho Altamira");
+    }
+    
+}
